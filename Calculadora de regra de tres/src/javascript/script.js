06f3b2ac0d68f1979e5f1bf6c4c111e5ad0897d7
Matheus Lima <128100927+MatheusLima2023@users.noreply.github.com>
2024-06-19 08:07:03 -0300

    Add files via upload

---
+++ Calculadora de regra de tres/src/javascript/script.js
@@ -0,0 +1,23 @@
+document.getElementById('calculete').addEventListener('click', function(){
+    const num1 = parseFloat(document.getElementById('num1').value);
+    const num2 = parseFloat(document.getElementById('num2').value);
+    const num3 = parseFloat(document.getElementById('num3').value);
+
+    if (isNaN(num1) || isNaN(num2) || isNaN(num3) || num1 === 0) {
+        document.getElementById('result').textContent = 'Ivalido';
+        
+        return
+    }
+
+    let result = (num3 * num2) / num1;
+
+    if (Number.isInteger(result)){
+        document.getElementById('result').textContent = result;
+    }
+
+    else{
+    
+        document.getElementById('result').textContent = result.toFixed(2);
+    }
+
+});
